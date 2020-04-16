ed4a087854b49fc88f7b4d1b24d8bbf0b88c07c1
Adam Li <adam2392@gmail.com>
2020-04-16 13:01:18 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Epilepsy automated Ablation and Resection Segmentation',
+    author='Adam Li, Diane Lee',
+    license='MIT',
+)
